92dd9878af47f0a83b671582bf8f7ac3062fe87f
mtdecaf <jcvan0116@gmail>
2024-12-16 20:49:01 -0800

    add layout container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,10 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const moonglade = localFont({
-  src: [{ path: "./fonts/moongladeLight.ttf",weight: "100", style: "normal" }, 
-    {path: "./fonts/moongladeRegular.ttf", weight: "400", style: "normal" },
-    {path:"./fonts/moongladeBold.ttf", weight: "700", style: "bold" },
+  src: [
+    { path: "./fonts/moongladeLight.ttf", weight: "100", style: "normal" },
+    { path: "./fonts/moongladeRegular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/moongladeBold.ttf", weight: "700", style: "bold" },
   ],
 });
 
@@ -21,8 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* Apply font to the body */}
-      <body className={`${moonglade.className} antialiased`}>{children}</body>
+      <body className={`${moonglade.className} antialiased object-center`}>
+        <div className="flex justify-center w-dvw h-dvh p-4 md:py-20 md:px-14 xl:px-auto">
+          <div className="border-8 border-black w-full xl:w-10/12 xl:max-w-screen-l h-full">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="">
+    <div className="home">
     </div>
   );
 }
